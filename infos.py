eebd0fb503fc5600fc1fae822d6637b875fb2c90
Will_tam <will_tam@club-internet.fr>
2018-08-18 08:45:42 +0200

    Becomes project under Eric6 IDE

---
+++ infos.py
@@ -0,0 +1,13 @@
+PRGNAME = "nctmenu"
+VERSION = "0.20180816"
+
+HOWTO = PRGNAME + """.py
+
+Usage:
+    """ + PRGNAME + """.py [options]
+
+Options:
+    -h, --help        This help.
+    -r, --reconf      Recreate configuration file.
+
+"""
--- nctmenu.py
+++ nctmenu.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+
 # Standard library import.
 import sys
+import os
+import argparse
+import hashlib
+import re
 
 # Third-part library import.
 
 # Project library import.
+import infos
 
 ######################
 
@@ -17,11 +23,19 @@ def main(arg):
     @return : 0 = all was good.
               ... = some problem occures.
     """
+    # check_args()
+    # config ... <- Use Class()
+    # verif_3rd_part_prg()
+    # verif_exec_other_paths()
+    # check_man_help ... <- Use Class()
+    # check_gfx_version ... <- Use Class()
+    # display()
+
     return 0
 
 ######################
 
 if __name__ == "__main__":
     rc = main(sys.argv[1:])      # Keep only the argus after the script name.
-    sys.exit(rc)
+    sys.exit(0)
 
